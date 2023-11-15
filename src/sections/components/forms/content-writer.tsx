09f8e3dd1afb298c4c3928cb0c1ed09aa54aad69
CPRToken <99814986+CPRToken@users.noreply.github.com>
2023-11-15 12:56:40 +1100

    spanish es 1st attempt

--- src/sections/components/forms/content-writer.tsx
+++ src/sections/components/forms/content-writer.tsx
@@ -11,6 +11,7 @@ import Slider from '@mui/material/Slider';
 import Paper from '@mui/material/Paper';
 import { tokens } from 'src/locales/tokens';
 import { useTranslation } from 'react-i18next';
+import CircularProgress from '@mui/material/CircularProgress';
 import useHandleSubmit from './handle-submit';
 
 
@@ -23,183 +24,174 @@ type Option = {
 
 
 const languageOptions: Option[] = [
-    { label: '', value: '' },
-  { label: 'English', value: 'english' },
-  { label: 'Spanish', value: 'spanish' },
-  { label: 'French', value: 'french' },
-  { label: 'German', value: 'german' },
-  { label: 'Italian', value: 'italian' },
-  { label: 'Portuguese', value: 'portuguese' },
-  { label: 'Russian', value: 'russian' },
-  { label: 'Chinese (Simplified)', value: 'chinese-simplified' },
-  { label: 'Chinese (Traditional)', value: 'chinese-traditional' },
-  { label: 'Japanese', value: 'japanese' },
-  { label: 'Korean', value: 'korean' },
-  { label: 'Arabic', value: 'arabic' },
-  { label: 'Hindi', value: 'hindi' },
-  { label: 'Turkish', value: 'turkish' },
-  { label: 'Dutch', value: 'dutch' },
-  { label: 'Swedish', value: 'swedish' },
-  { label: 'Finnish', value: 'finnish' },
-  { label: 'Danish', value: 'danish' },
-  { label: 'Norwegian', value: 'norwegian' },
-  { label: 'Polish', value: 'polish' },
-  { label: 'Greek', value: 'greek' },
-  { label: 'Hungarian', value: 'hungarian' },
-  { label: 'Czech', value: 'czech' },
-  { label: 'Slovak', value: 'slovak' },
-  { label: 'Romanian', value: 'romanian' },
-  { label: 'Bulgarian', value: 'bulgarian' },
-  { label: 'Serbian', value: 'serbian' },
-  { label: 'Croatian', value: 'croatian' },
-  { label: 'Bosnian', value: 'bosnian' },
-  { label: 'Slovenian', value: 'slovenian' },
-  { label: 'Albanian', value: 'albanian' },
-  { label: 'Lithuanian', value: 'lithuanian' },
-  { label: 'Latvian', value: 'latvian' },
-  { label: 'Estonian', value: 'estonian' },
-  { label: 'Maltese', value: 'maltese' },
-  { label: 'Thai', value: 'thai' },
-  { label: 'Filipino', value: 'filipino' },
-  { label: 'Vietnamese', value: 'vietnamese' },
-  { label: 'Indonesian', value: 'indonesian' },
-  { label: 'Malay', value: 'malay' },
-  { label: 'Persian', value: 'persian' },
-  { label: 'Hebrew', value: 'hebrew' },
-  { label: 'Urdu', value: 'urdu' },
-  { label: 'Swahili', value: 'swahili' },
-  { label: 'Yoruba', value: 'yoruba' },
-  { label: 'Igbo', value: 'igbo' },
-  { label: 'Zulu', value: 'zulu' },
-    // ... add more as needed
+  { label: '', value: '' },
+  { label: tokens.form.English, value: 'english' },
+  { label: tokens.form.Spanish, value: 'spanish' },
+  { label: tokens.form.French, value: 'french' },
+  { label: tokens.form.German, value: 'german' },
+  { label: tokens.form.Italian, value: 'italian' },
+  { label: tokens.form.Portuguese, value: 'portuguese' },
+  { label: tokens.form.Russian, value: 'russian' },
+  { label: tokens.form.Japanese, value: 'japanese' },
+  { label: tokens.form.Korean, value: 'korean' },
+  { label: tokens.form.Arabic, value: 'arabic' },
+  { label: tokens.form.Hindi, value: 'hindi' },
+  { label: tokens.form.Turkish, value: 'turkish' },
+  { label: tokens.form.Dutch, value: 'dutch' },
+  { label: tokens.form.Swedish, value: 'swedish' },
+  { label: tokens.form.Finnish, value: 'finnish' },
+  { label: tokens.form.Danish, value: 'danish' },
+  { label: tokens.form.Norwegian, value: 'norwegian' },
+  { label: tokens.form.Polish, value: 'polish' },
+  { label: tokens.form.Greek, value: 'greek' },
+  { label: tokens.form.Hungarian, value: 'hungarian' },
+  { label: tokens.form.Czech, value: 'czech' },
+  { label: tokens.form.Slovak, value: 'slovak' },
+  { label: tokens.form.Romanian, value: 'romanian' },
+  { label: tokens.form.Bulgarian, value: 'bulgarian' },
+  { label: tokens.form.Serbian, value: 'serbian' },
+  { label: tokens.form.Croatian, value: 'croatian' },
+  { label: tokens.form.Bosnian, value: 'bosnian' },
+  { label: tokens.form.Slovenian, value: 'slovenian' },
+  { label: tokens.form.Albanian, value: 'albanian' },
+  { label: tokens.form.Lithuanian, value: 'lithuanian' },
+  { label: tokens.form.Latvian, value: 'latvian' },
+  { label: tokens.form.Estonian, value: 'estonian' },
+  { label: tokens.form.Maltese, value: 'maltese' },
+  { label: tokens.form.Thai, value: 'thai' },
+  { label: tokens.form.Filipino, value: 'filipino' },
+  { label: tokens.form.Vietnamese, value: 'vietnamese' },
+  { label: tokens.form.Indonesian, value: 'indonesian' },
+  { label: tokens.form.Malay, value: 'malay' },
+  { label: tokens.form.Persian, value: 'persian' },
+  { label: tokens.form.Hebrew, value: 'hebrew' },
+  { label: tokens.form.Zulu, value: 'zulu' },
+  // ... add more as needed
 ];
+
 
 const styleOptions: Option[] = [
 
     { label: '', value: '' },
   { label: tokens.form.informative, value: 'informative' },
-  { label: 'Academic', value: 'academic' },
-  { label: 'Analytical', value: 'analytical' },
-  { label: 'Argumentative', value: 'argumentative' },
-  { label: 'Articulate', value: 'articulate' },
-  { label: 'Essay', value: 'essay' },
-  { label: 'Blog', value: 'blog' },
-  { label: 'Biographical', value: 'biographical' },
-  { label: 'Conversational', value: 'conversational' },
-  { label: 'Didactic', value: 'didactic' },
-  { label: 'Educational', value: 'educational' },
-  { label: 'Eloquent', value: 'eloquent' },
-  { label: 'Empathetic', value: 'empathetic' },
-  { label: 'Entertaining', value: 'entertaining' },
-  { label: 'Epic', value: 'epic' },
-  { label: 'Evocative', value: 'evocative' },
-  { label: 'Explanatory', value: 'explanatory' },
-  { label: 'Factual', value: 'factual' },
-  { label: 'Figurative', value: 'figurative' },
-  { label: 'Inspirational', value: 'inspirational' },
-  { label: 'Investigative', value: 'investigative' },
-  { label: 'Lyrical', value: 'lyrical' },
-  { label: 'Motivational', value: 'motivational' },
-  { label: 'Objective', value: 'objective' },
-  { label: 'Persuasive', value: 'persuasive' },
-  { label: 'Poetic', value: 'poetic' },
-  { label: 'Pragmatic', value: 'pragmatic' },
-  { label: 'Provocative', value: 'provocative' },
-  { label: 'Quirky', value: 'quirky' },
-  { label: 'Rhetorical', value: 'rhetorical' },
-  { label: 'Scholarly', value: 'scholarly' },
-  { label: 'Simplistic', value: 'simplistic' },
-  { label: 'Speculative', value: 'speculative' },
-  { label: 'Subjective', value: 'subjective' },
-  { label: 'Succinct', value: 'succinct' },
-  { label: 'Technical', value: 'technical' },
-    { label: 'Casual', value: 'casual' },
-    { label: 'Colloquial', value: 'colloquial' },
-  { label: 'Comparative', value: 'comparative' },
-  { label: 'Concise', value: 'concise' },
-    { label: 'Creative', value: 'creative' },
-  { label: 'Critical', value: 'critical' },
-  { label: 'Descriptive', value: 'descriptive' },
-  { label: 'Dramatic', value: 'dramatic' },
-  { label: 'Editorial', value: 'editorial' },
-  { label: 'Expository', value: 'expository' },
-  { label: 'Formal', value: 'formal' },
-  { label: 'Humorous', value: 'humorous' },
-  { label: 'Informative', value: 'informative' },
-  { label: 'Journalistic', value: 'journalistic' },
-  { label: 'Narrative', value: 'narrative' },
-  { label: 'Opinionated', value: 'opinionated' },
-  { label: 'Persuasive', value: 'persuasive' },
-  { label: 'Professional', value: 'professional' },
-  { label: 'Reflective', value: 'reflective' },
-  { label: 'Satirical', value: 'satirical' },
-  { label: 'Scientific', value: 'scientific' },
-  { label: 'SEO', value: 'seo' },
-  { label: 'Technical', value: 'technical' },
-  { label: 'Thought-Provoking', value: 'thought-provoking' },
-  { label: 'Vivid', value: 'vivid' },
+  { label: tokens.form.academic, value: 'academic' },
+  { label: tokens.form.analytical, value: 'analytical' },
+  { label: tokens.form.argumentative, value: 'argumentative' },
+  { label: tokens.form.articulate, value: 'articulate' },
+  { label: tokens.form.essay, value: 'essay' },
+  { label: tokens.form.blog, value: 'blog' },
+  { label: tokens.form.biographical, value: 'biographical' },
+  { label: tokens.form.conversational, value: 'conversational' },
+  { label: tokens.form.Didactic, value: 'didactic' },
+  { label: tokens.form.Educational, value: 'educational' },
+  { label: tokens.form.Eloquent, value: 'eloquent' },
+  { label: tokens.form.Empathetic, value: 'empathetic' },
+  { label: tokens.form.Entertaining, value: 'entertaining' },
+  { label: tokens.form.Epic, value: 'epic' },
+  { label: tokens.form.Evocative, value: 'evocative' },
+  { label: tokens.form.Explanatory, value: 'explanatory' },
+  { label: tokens.form.Factual, value: 'factual' },
+  { label: tokens.form.Figurative, value: 'figurative' },
+  { label: tokens.form.Inspirational, value: 'inspirational' },
+  { label: tokens.form.Investigative, value: 'investigative' },
+  { label: tokens.form.Lyrical, value: 'lyrical' },
+  { label: tokens.form.Motivational, value: 'motivational' },
+  { label: tokens.form.Objective, value: 'objective' },
+  { label: tokens.form.Persuasive, value: 'persuasive' },
+  { label: tokens.form.Poetic, value: 'poetic' },
+  { label: tokens.form.Pragmatic, value: 'pragmatic' },
+  { label: tokens.form.Provocative, value: 'provocative' },
+  { label: tokens.form.Quirky, value: 'quirky' },
+  { label: tokens.form.Rhetorical, value: 'rhetorical' },
+  { label: tokens.form.Scholarly, value: 'scholarly' },
+  { label: tokens.form.Simplistic, value: 'simplistic' },
+  { label: tokens.form.Speculative, value: 'speculative' },
+  { label: tokens.form.Subjective, value: 'subjective' },
+  { label: tokens.form.Succinct, value: 'succinct' },
+  { label: tokens.form.Technical, value: 'technical' },
+  { label: tokens.form.Casual, value: 'casual' },
+  { label: tokens.form.Colloquial, value: 'colloquial' },
+  { label: tokens.form.Comparative, value: 'comparative' },
+  { label: tokens.form.Concise, value: 'concise' },
+  { label: tokens.form.Creative, value: 'creative' },
+  { label: tokens.form.Critical, value: 'critical' },
+  { label: tokens.form.Descriptive, value: 'descriptive' },
+  { label: tokens.form.Dramatic, value: 'dramatic' },
+  { label: tokens.form.Editorial, value: 'editorial' },
+  { label: tokens.form.Expository, value: 'expository' },
+  { label: tokens.form.Formal, value: 'formal' },
+  { label: tokens.form.Humorous, value: 'humorous' },
+  { label: tokens.form.Informative, value: 'informative' },
+  { label: tokens.form.Journalistic, value: 'journalistic' },
+  { label: tokens.form.Narrative, value: 'narrative' },
+  { label: tokens.form.Opinionated, value: 'opinionated' },
+  { label: tokens.form.Persuasive, value: 'persuasive' },
+  { label: tokens.form.Professional, value: 'professional' },
+  { label: tokens.form.Reflective, value: 'reflective' },
+  { label: tokens.form.Satirical, value: 'satirical' },
+  { label: tokens.form.Scientific, value: 'scientific' },
+  { label: tokens.form.SEO, value: 'seo' },
+  { label: tokens.form.Technical, value: 'technical' },
+
+  { label: tokens.form.Vivid, value: 'vivid' },
 
     // ... add more as needed
 ];
 
 const toneOptions: Option[] = [
-    { label: '', value: '' },
-    { label: 'Formal', value: 'formal' },
-  { label: 'Assertive', value: 'assertive' },
-  { label: 'Friendly', value: 'friendly' },
-  { label: 'Informal', value: 'informal' },
-  { label: 'Inspiring', value: 'inspiring' },
-  { label: 'Professional', value: 'professional' },
-  { label: 'Warm', value: 'warm' },
-  { label: 'Candid', value: 'candid' },
-  { label: 'Cheerful', value: 'cheerful' },
-  { label: 'Compassionate', value: 'compassionate' },
-  { label: 'Confident', value: 'confident' },
-  { label: 'Conversational', value: 'conversational' },
-  { label: 'Encouraging', value: 'encouraging' },
-  { label: 'Enthusiastic', value: 'enthusiastic' },
-  { label: 'Honest', value: 'honest' },
-  { label: 'Hopeful', value: 'hopeful' },
-  { label: 'Humorous', value: 'humorous' },
-  { label: 'Inquisitive', value: 'inquisitive' },
-  { label: 'Intimate', value: 'intimate' },
-  { label: 'Lighthearted', value: 'lighthearted' },
-  { label: 'Optimistic', value: 'optimistic' },
-  { label: 'Passionate', value: 'passionate' },
-  { label: 'Placid', value: 'placid' },
-  { label: 'Playful', value: 'playful' },
-  { label: 'Poignant', value: 'poignant' },
-  { label: 'Provocative', value: 'provocative' },
-  { label: 'Reassuring', value: 'reassuring' },
-  { label: 'Reflective', value: 'reflective' },
-  { label: 'Respectful', value: 'respectful' },
-  { label: 'Reverent', value: 'reverent' },
-  { label: 'Sarcastic', value: 'sarcastic' },
-  { label: 'Sincere', value: 'sincere' },
-  { label: 'Solemn', value: 'solemn' },
-  { label: 'Thoughtful', value: 'thoughtful' },
-  { label: 'Uplifting', value: 'uplifting' },
-  { label: 'Whimsical', value: 'whimsical' },
-  { label: 'Witty', value: 'witty' },
-  { label: 'Zealous', value: 'zealous' },
-  { label: 'Neutral', value: 'neutral' },
-  { label: 'Melancholic', value: 'melancholic' },
-  { label: 'Eloquent', value: 'eloquent' },
-  { label: 'Empathetic', value: 'empathetic' },
-  { label: 'Gentle', value: 'gentle' },
-  // ... add more as needed
-
-
-  // ... add more as needed
+  { label: '', value: '' },
+    { label: tokens.form.Formal, value: 'formal' },
+    { label: tokens.form.Assertive, value: 'assertive' },
+    { label: tokens.form.Friendly, value: 'friendly' },
+    { label: tokens.form.Informal, value: 'informal' },
+    { label: tokens.form.Inspiring, value: 'inspiring' },
+    { label: tokens.form.Professional, value: 'professional' },
+    { label: tokens.form.Warm, value: 'warm' },
+    { label: tokens.form.Candid, value: 'candid' },
+    { label: tokens.form.Cheerful, value: 'cheerful' },
+    { label: tokens.form.Compassionate, value: 'compassionate' },
+    { label: tokens.form.Confident, value: 'confident' },
+    { label: tokens.form.Conversational, value: 'conversational' },
+    { label: tokens.form.Encouraging, value: 'encouraging' },
+    { label: tokens.form.Enthusiastic, value: 'enthusiastic' },
+    { label: tokens.form.Honest, value: 'honest' },
+    { label: tokens.form.Hopeful, value: 'hopeful' },
+    { label: tokens.form.Humorous, value: 'humorous' },
+    { label: tokens.form.Inquisitive, value: 'inquisitive' },
+    { label: tokens.form.Intimate, value: 'intimate' },
+    { label: tokens.form.Lighthearted, value: 'lighthearted' },
+    { label: tokens.form.Optimistic, value: 'optimistic' },
+    { label: tokens.form.Passionate, value: 'passionate' },
+    { label: tokens.form.Placid, value: 'placid' },
+    { label: tokens.form.Playful, value: 'playful' },
+    { label: tokens.form.Poignant, value: 'poignant' },
+    { label: tokens.form.Provocative, value: 'provocative' },
+    { label: tokens.form.Reassuring, value: 'reassuring' },
+    { label: tokens.form.Reflective, value: 'reflective' },
+    { label: tokens.form.Respectful, value: 'respectful' },
+    { label: tokens.form.Reverent, value: 'reverent' },
+    { label: tokens.form.Sarcastic, value: 'sarcastic' },
+    { label: tokens.form.Sincere, value: 'sincere' },
+    { label: tokens.form.Solemn, value: 'solemn' },
+    { label: tokens.form.Thoughtful, value: 'thoughtful' },
+    { label: tokens.form.Uplifting, value: 'uplifting' },
+    { label: tokens.form.Whimsical, value: 'whimsical' },
+    { label: tokens.form.Witty, value: 'witty' },
+    { label: tokens.form.Zealous, value: 'zealous' },
+    { label: tokens.form.Neutral, value: 'neutral' },
+    { label: tokens.form.Melancholic, value: 'melancholic' },
+    { label: tokens.form.Eloquent, value: 'eloquent' },
+    { label: tokens.form.Empathetic, value: 'empathetic' },
+    { label: tokens.form.Gentle, value: 'gentle' },
+    // ... add more as needed
 ];
 
-// ... rest of the code remains the same
 
 
 
 export const ContentWriter: FC = () => {
   const { t } = useTranslation();
-  const { handleSubmit, openAIResponse } = useHandleSubmit();
+  const { handleSubmit, openAIResponse, isLoading } = useHandleSubmit();
   const [language, setLanguage] = useState<string>('');
   const [style, setStyle] = useState<string>('');
   const [tone, setTone] = useState<string>('');
@@ -214,7 +206,7 @@ export const ContentWriter: FC = () => {
 
   const submitToOpenAI = () => {
     // Construct a prompt that OpenAI can use to generate an article
-    const newPrompt = `Write a ${duration}-word article titled "${title}" in ${language} ${t(style)} style and ${t(tone)} tone, using the following keywords: ${keywords}`;
+    const newPrompt = `Write a ${duration}-word article titled "${title}" in ${t(language)} ${t(style)} style and ${t(tone)} tone, using the following keywords: ${keywords}`;
     setPrompt(newPrompt); // Update the prompt state
     handleSubmit(newPrompt, maxTokensForResume)
         .then(() => {
@@ -281,10 +273,11 @@ export const ContentWriter: FC = () => {
         >
           {languageOptions.map((option) => (
             <option key={option.value} value={option.value}>
-              {option.label}
+              {t(option.label)} {/* Apply translation here */}
             </option>
           ))}
         </TextField>
+
         <TextField
           fullWidth
           label={t(tokens.form.style)}
@@ -311,10 +304,11 @@ export const ContentWriter: FC = () => {
         >
           {toneOptions.map((option) => (
             <option key={option.value} value={option.value}>
-              {option.label}
+              {t(option.label)} {/* Apply translation here */}
             </option>
           ))}
         </TextField>
+
         <div>
           <label>{t(tokens.form.words)}</label>
           <Slider
@@ -334,10 +328,17 @@ export const ContentWriter: FC = () => {
       </Stack>
       <Box sx={{ mt: 3 }}>
         {/* Button to submit the prompt to OpenAI */}
-        <Button onClick={submitToOpenAI} type="submit" variant="contained" fullWidth>
-          Submit
+        <Button
+          onClick={submitToOpenAI}
+          type="submit"
+          variant="contained"
+          fullWidth
+          disabled={isLoading}  // Disable the button while loading
+        >
+          {isLoading ? <CircularProgress size={24} /> : 'Submit'}
         </Button>
       </Box>
+
       {openAIResponse && (
         <Box sx={{ mt: 3 }}>
           <label>Your Content:</label>
