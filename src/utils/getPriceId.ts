05dc2a3be03bdf8b9ee950421afde35d0826d291
CPRToken <99814986+CPRToken@users.noreply.github.com>
2024-08-07 00:24:18 +0700

    priceupdate2

--- src/utils/getPriceId.ts
+++ src/utils/getPriceId.ts
@@ -1,11 +1,11 @@
 // utils/getPriceId.ts
 export const getPriceId = (plan: string): string => {
-  if (plan === 'Basic') return process.env.LIVE_PRICE_BASIC!;
-  if (plan === 'Premium') return process.env.LIVE_PRICE_PREMIUM!;
-  if (plan === 'Business') return process.env.LIVE_PRICE_BUSINESS!;
-  if (plan === 'BasicYearly') return process.env.LIVE_PRICE_BASIC_YEARLY!;
-  if (plan === 'PremiumYearly') return process.env.LIVE_PRICE_PREMIUM_YEARLY!;
-  if (plan === 'BusinessYearly') return process.env.LIVE_PRICE_BUSINESS_YEARLY!;
+  if (plan === 'Basic') return 'price_1Pk4zmI7exj9oAo9khc4OT16';
+  if (plan === 'Premium') return 'price_1Pk4zkI7exj9oAo9N92hGKqe';
+  if (plan === 'Business') return 'price_1Pk4ziI7exj9oAo95ZIL3sby';
+  if (plan === 'BasicYearly') return 'price_1Pk4zgI7exj9oAo9DSyIUy8G';
+  if (plan === 'PremiumYearly') return 'price_1Pk4zeI7exj9oAo9eUPovxQl';
+  if (plan === 'BusinessYearly') return 'price_1Pk4zbI7exj9oAo9qsyipPNj';
 
   throw new Error(`Unknown plan: ${plan}`);
 };
