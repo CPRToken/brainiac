587ef4521a7840c457aa92bbe684e85a5e3f125c
CPRToken <jasonengtoday@gmail.com>
2025-04-12 18:11:59 +1000

    affliate2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,26 @@
+//src/pages/index.tsx
 import type { NextPage } from 'next';
 import { Seo } from 'src/components/seo';
 import { usePageView } from 'src/hooks/use-page-view';
 import { Layout as MarketingLayout } from 'src/layouts/marketing';
-
 import { HomeCta } from 'src/sections/home/home-cta';
 import { HomeHero } from 'src/sections/home/home-hero';
 import PricingSection from  'src/pages/pricing'
-
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 
 const Page: NextPage = () => {
     usePageView();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const ref = router.query.ref;
+    if (ref) {
+      localStorage.setItem('referrer', ref as string);
+    }
+  }, [router.query.ref]);
 
   return (
     <>
